82d6a7e2d70fa6e24724eb2a6a8c7e22a4fdb6f0
chenkuilin <chenkuilin@mail.fw-iot.com>
2019-11-14 18:50:42 +0800

    添加大兄弟

---
+++ src/main/com/wolian/java/Demo.java
@@ -0,0 +1,12 @@
+package main.com.wolian.java;
+
+/**
+ * @author chenkuilin
+ * @date 2019/11/14 18:09
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("大兄弟");
+    }
+}
