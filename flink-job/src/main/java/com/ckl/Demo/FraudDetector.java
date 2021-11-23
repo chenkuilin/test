411f35ebbd6d6a2ad2810859d27d7b69b3e46a81
rscChenkuilin <chenkuilinckl@163.com>
2021-11-23 15:49:25 +0800

    新增一个flink测试项目

---
+++ flink-job/src/main/java/com/ckl/Demo/FraudDetector.java
@@ -0,0 +1,35 @@
+package com.ckl.Demo;
+
+
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.util.Collector;
+import org.apache.flink.walkthrough.common.entity.Alert;
+import org.apache.flink.walkthrough.common.entity.Transaction;
+
+/**
+ * @author chenkuilin
+ * @date 2021/11/23
+ * @desc
+ */
+public class FraudDetector extends KeyedProcessFunction<Long, Transaction, Alert> {
+
+    private static final double SMALL_AMOUNT = 1.00;
+    private static final double LARGE_AMOUNT = 500.00;
+    private static final long ONE_MINUTE = 60 * 1000;
+
+    @Override
+    public void processElement(
+            Transaction transaction,
+            Context context,
+            Collector<Alert> collector) throws Exception {
+
+        Alert alert = new Alert();
+        alert.setId(transaction.getAccountId());
+
+        collector.collect(alert);
+    }
+}
